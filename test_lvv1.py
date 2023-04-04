4f61d0f24750d88d4c4e6e0f12ef9bb53158760d
Dongsun Kim <darkrsw@gmail.com>
2023-04-04 20:42:45 +0900

    Update test_lvv1.py

--- test_lvv1.py
+++ test_lvv1.py
@@ -7,7 +7,7 @@ def test_lvv_sha256():
         encoded = "mysalt".encode()
         generate_hash(encoded).hex()
 
-    answer = {'t': 128, 'a': 65, 'b': 65, 'c': 65, 'd': 65, 'e': 65, 'f': 65, 'g': 65, 'h': 65, 't1': 64, 't2': 64, 'term1': 48, 'schedule': 48, 'term2': 43, 'term3': 36, 'term4': 36, 'h0': 2, 'h1': 2, 'h2': 2, 'h3': 2, 'h5': 2, 'h4': 2, 'h6': 2, 'h7': 2, 'message': 1, 'length': 1, 'blocks': 1, 'i': 1, 'message_block': 1, 'message_schedule': 1}
+    answer = {'t': 128, 'message_schedule': 65, 'a': 65, 'b': 65, 'c': 65, 'd': 65, 'e': 65, 'f': 65, 'g': 65, 'h': 65, 't1': 64, 't2': 64, 'message': 52, 'term1': 48, 'schedule': 48, 'term2': 43, 'term3': 36, 'term4': 36, 'blocks': 2, 'h0': 2, 'h1': 2, 'h2': 2, 'h3': 2, 'h5': 2, 'h4': 2, 'h6': 2, 'h7': 2, 'length': 1, 'i': 1, 'message_block': 1}
     assert traced.getLVVmap() == answer
 
 def test_lvv_md5():
